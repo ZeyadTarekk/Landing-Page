c21a6ed6f6ac18809d6541155e31f227ded7c47c
Zeyad Tarek <zeyad.ta01@gmail.com>
2021-11-23 05:16:40 +0200

    File changes

--- js/app.js
+++ js/app.js
@@ -35,13 +35,15 @@ const ulNavBar = document.getElementById("navbar__list");
 
 function isInViewport(element) {
   const rect = element.getBoundingClientRect();
-  return (
+  if (
     rect.top >= 0 &&
     rect.left >= 0 &&
     rect.bottom <=
       (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
+  )
+    return true;
+  else return false;
 }
 
 /**
